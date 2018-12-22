46c5367ba152015538e1a2542daf622975dbf089
Pedro <pedro.guimaraes@canonical.com>
2018-12-22 08:42:00 +0100

    minor corrections

--- hooks/hooks.py
+++ hooks/hooks.py
@@ -21,7 +21,9 @@ from subprocess import check_call
 from charmhelpers.core import unitdata
 
 from charmhelpers.core.hookenv import (
+    config as config_get,
     Hooks,
+    UnregisteredHookError,
     config,
     log,
     DEBUG,
@@ -32,6 +34,8 @@ from charmhelpers.core.hookenv import (
     relation_set,
     related_units,
     status_set,
+    open_port,
+    is_leader,
     relation_id,
 )
 
@@ -80,9 +84,9 @@ def config_changed():
     for r_id in relation_ids('ha'):
         ha_joined(relation_id=r_id)
     if config_get("mode") is "balancer":
-        status_set("Ready","Now balancing...")
+        status_set("active","Now balancing...")
     else:
-        status_set("Ready", "Now proxying")
+        status_set("active", "Now proxying")
 
 
 # TODO: build this function, choose a relation like haproxy charm
@@ -97,7 +101,13 @@ def convert_ports_config():
             port_ints.extend(range(int(p.split(":")[0]),int(p.split(":")[-1])))
         else:
             port_ints.append(int(p))
-    return port_ints.sort()
+    port_ints.sort()
+    return port_ints
+
+
+def block_service(msg):
+    status_set("blocked",msg)
+    service_stop("haproxy")
 
 
 def config_proxy():
@@ -110,20 +120,40 @@ def config_proxy():
 
     with open("/etc/haproxy/haproxy.cfg","w") as f:
         cfg=[]
+        pcount=0
+        if not port_ints:
+            log("ports not defined, stopping...")
+            block_service("proxy_port undefined, please define this config")
+            return
         for b in config_get("backend_list").split(","):
+            if pcount >= len(port_ints):
+                log("WARNING: not enough port supplied")
+                block_service("Not enough ports supplied")
+                f.close()
+                return
             cfg.append(fe_part.render(bind_port=port_ints[pcount],
-                                      service_name="service_%s".format(pcount),
+                                      service_name="service_{}".format(pcount),
                                       backend=b))
+            pcount+=1
         f.write("\n\n\n".join(cfg))
         f.close()
-
     service_restart("haproxy")
 
 
 @hooks.hook('ha-relation-joined')
 def ha_joined(relation_id=None):
     settings = generate_ha_relation_data('hpy')
+    log("ha_joined: generated ha relation data is {}".format(settings))
     relation_set(relation_id=relation_id, **settings)
 
 ## TODO: add ha-relation-changed as charm-keystone, for example
 
+def main():
+    try:
+        hooks.execute(sys.argv)
+    except UnregisteredHookError as e:
+        log('Unknown hook {} - skipping.'.format(e))
+
+
+if __name__ == '__main__':
+    main()
